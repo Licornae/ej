317549967ca5ea5362ac66f7cf8c0b2da9cbdf01
Caroline <bergere.caroline@outlook.fr>
2025-01-27 16:05:16 +0100

    Premiere utilisation de fonction

--- ej/src/ej/Main.java
+++ ej/src/ej/Main.java
@@ -3,10 +3,13 @@ package ej;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		String texte = "Hello World !";
-		System.out.println(texte);
-
+		int somme = Main.addition(4, 4);
+		System.out.println(somme);
 	}
+	
+	public static int addition(final int entier1, final int entier2) {
+		return entier1 + entier2;
+	}
+	
 
 }
