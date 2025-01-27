91e050685aa206ebbfc3ecad714f258c5af06354
Caroline <bergere.caroline@outlook.fr>
2025-01-27 16:39:34 +0100

    Premiere utilisation de switch + lecture des réponses dans la console

--- ej/src/ej/Main.java
+++ ej/src/ej/Main.java
@@ -1,13 +1,31 @@
 package ej;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		affichage("Marty");
+		System.out.println("Indiquez la météo (soleil, nuage ou pluie) : ");
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	        String reponse;
+			reponse = reader.readLine();
+	        commenteLaMeteo(reponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
 	}
 	
-	public static void affichage(String text){
-		System.out.println("Bonjour " + text);
+	public static void commenteLaMeteo(final String meteo) {
+		switch(meteo) {
+		case "soleil" -> System.out.println("Beau temps");
+		case "nuage" -> System.out.println("Couvert");
+		case "pluie" -> System.out.println("Mauvais temps");
+		default -> System.out.println("Je ne sais pas.");
+		}
 	}
 	
 
