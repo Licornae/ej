abec7c4d36b0613ead02ec84683f97dfb4f20885
Caroline <bergere.caroline@outlook.fr>
2025-01-28 15:59:04 +0100

    Injection de dépendance

--- ej/src/ej/Main.java
+++ ej/src/ej/Main.java
@@ -5,9 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		IBloc unBlocMur = new Mur(10,10,5,true);
 		IBloc unBlocPorte = new Porte(1, 1, 1, false);
+		IBloc unBlocMurRempart = new Mur(100,100,100,true);
 		
 		unBlocMur.afficherDescription();
 		unBlocPorte.afficherDescription();
+		
+		Rempart monRempart = new Rempart(unBlocMurRempart);
 		
 	}
 
