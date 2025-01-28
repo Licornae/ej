c31a0b8c38746347ac3916eb197c93a6536c2cce
Caroline <bergere.caroline@outlook.fr>
2025-01-28 10:45:52 +0100

    Ecrire sa première classe - Bloc

---
+++ ej/src/ej/Bloc.java
@@ -0,0 +1,15 @@
+package ej;
+
+public class Bloc {
+	
+	String description;
+	
+	Bloc(){
+		this.description = "Ceci est un bloc du jeu Epicraft's Journey";
+	}
+	
+	void afficherDescription() {
+		System.out.println(description);
+	}
+
+}
--- ej/src/ej/Main.java
+++ ej/src/ej/Main.java
@@ -2,19 +2,9 @@ package ej;
 
 public class Main {
 	
-	public static void main(String[] args) {  //Compter le nombre de 0 présents dans le tableau
-		
-		int[] unTableauDEntier = {26, 0, 1985, 0, 12, 11, 1955, 0, 0};
-		int cpt = 0;
-		
-		for(int i = 0; i <= unTableauDEntier.length -1; i++) {
-			if (unTableauDEntier[i] == 0) {	
-				cpt ++;
-			}		
-		}
-
-		System.out.println(cpt);
-		
+	public static void main(String[] args) {  
+		Bloc unBloc = new Bloc();
+		unBloc.afficherDescription();
 	}
 	
 }
